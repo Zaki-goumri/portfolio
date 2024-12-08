4b9c88f935933b19b8c8f1b02f493ff4d4b79920
zaki <z.goumri@esi-sba.dz>
2024-12-08 23:08:29 +0100

    remove unused imports

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -1,6 +1,4 @@
 'use client'
-
-import Image from 'next/image'
 import { Button } from '@/components/ui/button'
 import { Github, Linkedin, Instagram } from 'lucide-react'
 import { TypeAnimation } from 'react-type-animation'
